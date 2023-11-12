a56ef61f83d1f585eedc1053c3f3ba4216ed5f56
Shehneel Khan <shehneelkhan92@gmail.com>
2023-11-12 15:52:11 +0500

    Updated database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:admin@localhost:3306/ecommerce'
+URL_DATABASE = 'mysql+pymysql://<username>:<password>@localhost:3306/<database_name>'
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
